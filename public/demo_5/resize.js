18fe0636bc7bf9271b467056a265ec2d0848d900
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-29 13:12:04 +0000

    Further refactoring of resize.js

--- public/demo_5/resize.js
+++ public/demo_5/resize.js
@@ -2,17 +2,14 @@ $(document).ready(function() {
 	var phi = (1 + Math.sqrt(5))/2;
 
 	$(window).resize(function() {
-		var goldenWidth, goldenHeight,
-			windowWidth  = $(this).width(),
-			windowHeight = $(this).height();
+		var goldenWidth  = windowWidth  = $(this).width(),
+			goldenHeight = windowHeight = $(this).height();
 
 		if (windowWidth/windowHeight > phi) {
 			// panoramic - use full height
 			goldenWidth  = windowHeight * phi;
-			goldenHeight = windowHeight;
 		} else {
 			// portrait  - use full width
-			goldenWidth  = windowWidth;
 			goldenHeight = windowWidth / phi;
 		};
 
