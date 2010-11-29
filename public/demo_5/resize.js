bc2c338d646a0000d70ac43235dde3df01b2c395
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-29 13:13:17 +0000

    Reformatted resize.js to use spaces (not tabs)

--- public/demo_5/resize.js
+++ public/demo_5/resize.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-	var phi = (1 + Math.sqrt(5))/2;
+    var phi = (1 + Math.sqrt(5))/2;
 
-	$(window).resize(function() {
-		var goldenWidth  = windowWidth  = $(this).width(),
-			goldenHeight = windowHeight = $(this).height();
+    $(window).resize(function() {
+        var goldenWidth  = windowWidth  = $(this).width(),
+            goldenHeight = windowHeight = $(this).height();
 
-		if (windowWidth/windowHeight > phi) {
-			// panoramic - use full height
-			goldenWidth  = windowHeight * phi;
-		} else {
-			// portrait  - use full width
-			goldenHeight = windowWidth / phi;
-		};
+        if (windowWidth/windowHeight > phi) {
+            // panoramic - use full height
+            goldenWidth  = windowHeight * phi;
+        } else {
+            // portrait  - use full width
+            goldenHeight = windowWidth / phi;
+        };
 
-		$("#container > div.cycle")
-			.width(goldenWidth)
-			.height(goldenHeight);
-	}).resize();
+        $("#container > div.cycle")
+            .width(goldenWidth)
+            .height(goldenHeight);
+    }).resize();
 
 });
