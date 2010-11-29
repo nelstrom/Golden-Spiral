c31add71675e6bc256bb7f3023e0df2356b2210b
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-29 21:11:11 +0000

    Reformatted using 2 spaces.

--- public/javascripts/resize.js
+++ public/javascripts/resize.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-	var phi = (1 + Math.sqrt(5))/2;
+  var phi = (1 + Math.sqrt(5))/2;
 
-	$(window).resize(function() {
-		var width = $(this).width(),
-				height = $(this).height(),
-				phi = (1 + Math.sqrt(5))/2;
+  $(window).resize(function() {
+    var width = $(this).width(),
+        height = $(this).height(),
+        phi = (1 + Math.sqrt(5))/2;
 
-		if (width/height > phi) {
-			// letterbox - use full available height
-			var calculated_width = height * phi;
-			$("#container > div.cycle")
-				.height(height)
-				.width(calculated_width);
-		} else {
-			// panoramic portrait - use full available width
-			var calculated_height = width / phi;
-			$("#container > div.cycle")
-				.width(width)
-				.height(calculated_height);
-		};
-	}).resize();
+    if (width/height > phi) {
+      // letterbox - use full available height
+      var calculated_width = height * phi;
+      $("#container > div.cycle")
+        .height(height)
+        .width(calculated_width);
+    } else {
+      // panoramic portrait - use full available width
+      var calculated_height = width / phi;
+      $("#container > div.cycle")
+        .width(width)
+        .height(calculated_height);
+    };
+  }).resize();
 });
