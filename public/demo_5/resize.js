2abe6c52205fc4fa38c7a8df3dde1d4c19e0d2ea
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-29 21:13:13 +0000

    Change from 4 spaces to 2 spaces for indentation.

--- public/demo_5/resize.js
+++ public/demo_5/resize.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
-    var phi = (1 + Math.sqrt(5))/2;
+  var phi = (1 + Math.sqrt(5))/2;
 
-    $(window).resize(function() {
-        var goldenWidth  = windowWidth  = $(this).width(),
-            goldenHeight = windowHeight = $(this).height();
+  $(window).resize(function() {
+    var goldenWidth  = windowWidth  = $(this).width(),
+        goldenHeight = windowHeight = $(this).height();
 
-        if (windowWidth/windowHeight > phi) {
-            // panoramic - use full height
-            goldenWidth  = windowHeight * phi;
-        } else {
-            // portrait  - use full width
-            goldenHeight = windowWidth / phi;
-        };
+    if (windowWidth/windowHeight > phi) {
+      // panoramic viewport - use full height
+      goldenWidth  = windowHeight * phi;
+    } else {
+      // portrait viewport  - use full width
+      goldenHeight = windowWidth / phi;
+    };
 
-        $("#container > div.cycle")
-            .width(goldenWidth)
-            .height(goldenHeight);
-    }).resize();
+    $("#container > div.cycle")
+      .width(goldenWidth)
+      .height(goldenHeight);
+
+  }).resize();
 
 });
